949c16d95aa90b264356dd8dd68111a3b48cca69
VenkatNan <nanditavenkat@gmail.com>
2020-11-19 10:01:22 -0600

    some changes

--- js/app.js
+++ js/app.js
@@ -1,24 +1,16 @@
+
 const result = document.getElementById('output');
 const input = document.getElementById('userInput');
 
 //on add btn click, take input, add to output
 const plusBtn = document.getElementById('addBtn');
-
 plusBtn.addEventListener('click',()=>{
     result.value = Number(input.value) + Number(result.value);
 });
+
 //on minus btn click, take input, subtract from output
 const minusBtn = document.getElementById('minusBtn');
 
 minusBtn.addEventListener('click',()=>{
     result.value = Number(result.value) - Number(input.value);
 });
-
-function outputColor{
-    if(result.value > 0){
-
-    }
-    if(result.value < 0){
-        
-    }
-}
